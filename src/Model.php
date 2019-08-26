a4e5ffc760fb8bf6c6987b5da89e3033f8bdba3f
Andrew McLagan <andrewmclagan@gmail.com>
2019-08-26 16:30:20 +1000

    html decode gone

--- src/Model.php
+++ src/Model.php
@@ -300,7 +300,7 @@ class Model implements Arrayable
                     $data[$key] = Types::cast($instance->$key, $instance->cast[$key]);
                 }
             } else {
-                $data[$key] = html_entity_decode($instance->$key);
+                $data[$key] = $instance->$key;
             }
         }
 
